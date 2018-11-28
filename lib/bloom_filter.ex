8e0c0c4223479f1b8ff62ff24302afa46a4950d2
pt-repos <pulkit.tripathi@outlook.com>
2018-11-28 00:34:30 -0500

    added docs

--- lib/bloom_filter.ex
+++ lib/bloom_filter.ex
@@ -1,4 +1,13 @@
 defmodule BloomFilter do
+  @moduledoc """
+  BloomFilter implementation for quick probabilistic searches.
+  The data structure can quickly make a probabilistic guess whether
+  an item exists in the set or not, but never gives a false negative.
+  """
+
+  @doc """
+  Initiates a Bloom Filter, sets the required bit size.
+  """
   def init(expected_size, error_rate) do
     hash_fns = [:sha, :ripemd160, :sha256]
     m = calculate_required_bits(expected_size, error_rate)
@@ -6,12 +15,20 @@ defmodule BloomFilter do
     [bits: bits, length: m, hash_fns: hash_fns]
   end
 
+  @doc """
+  Puts the given list of items in the filter, i.e. sets the bits corresponding 
+  to each item's hash values in the bit array to 1. 
+  """
   def put(filter, items) when is_list(items) do
     Enum.reduce(items, filter, fn item, acc ->
       put(acc, item)
     end)
   end
 
+  @doc """
+  Puts the given item in the filter ,i.e. sets the bits corresponding 
+  to item's hash values in the bit array to 1.
+  """
   def put(filter, item) do
     bits =
       hash(item, filter[:hash_fns], filter[:length])
@@ -22,21 +39,29 @@ defmodule BloomFilter do
     Keyword.put(filter, :bits, bits)
   end
 
+  @doc """
+  Checks whether each item in the given item probably exists in the filter.
+  """
   def contains?(filter, items) when is_list(items) do
     Enum.all?(items, fn item -> contains?(filter, item) end)
   end
 
+  @doc """
+  Checks whether the given item probably exists in the filter.
+  """
   def contains?(filter, item) do
     hash(item, filter[:hash_fns], filter[:length])
     |> Enum.all?(fn x -> Enum.at(filter[:bits], x) == 1 end)
   end
 
+  # Calculates the required size of bit array for the given parameters.
   defp calculate_required_bits(expected_size, error_rate) do
     n = expected_size
     p = error_rate
     round(-n * :math.log(p) / :math.pow(:math.log(2), 2))
   end
 
+  # Calculates the bits to be set for the given item
   defp hash(item, hash_fns, length) do
     Enum.map(hash_fns, fn hash_fn ->
       hash1 =
