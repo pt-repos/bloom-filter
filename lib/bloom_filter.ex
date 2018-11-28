3aef59541e4c92f8ed4b15708117b3b20e5346ff
pt-repos <pulkit.tripathi@outlook.com>
2018-11-27 20:01:23 -0500

    initial commit

---
+++ lib/bloom_filter.ex
@@ -0,0 +1,18 @@
+defmodule BloomFilter do
+  @moduledoc """
+  Documentation for BloomFilter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BloomFilter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bloom_filter_test.exs
@@ -0,0 +1,8 @@
+defmodule BloomFilterTest do
+  use ExUnit.Case
+  doctest BloomFilter
+
+  test "greets the world" do
+    assert BloomFilter.hello() == :world
+  end
+end
