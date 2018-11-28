4d36e8d7f87ef71abff9d5fc85d5ca72862c8657
pt-repos <pulkit.tripathi@outlook.com>
2018-11-27 20:03:54 -0500

    added bloom filter with init, put and contains? funtionalities and tests.

--- test/bloom_filter_test.exs
+++ test/bloom_filter_test.exs
@@ -2,7 +2,28 @@ defmodule BloomFilterTest do
   use ExUnit.Case
   doctest BloomFilter
 
-  test "greets the world" do
-    assert BloomFilter.hello() == :world
+  @size 10
+  @rate 0.3
+  @length 25
+
+  test "initialize" do
+    filter = BloomFilter.init(@size, @rate)
+    assert !Enum.empty?(filter)
+    assert !Enum.empty?(filter[:bits])
+    assert filter[:length] == @length
+  end
+
+  test "filter contains item" do
+    filter = BloomFilter.init(@size, @rate)
+    filter = BloomFilter.put(filter, "test")
+
+    assert BloomFilter.contains?(filter, "test")
+  end
+
+  test "filter doesn't contain item" do
+    filter = BloomFilter.init(@size, @rate)
+    filter = BloomFilter.put(filter, "test")
+
+    assert !BloomFilter.contains?(filter, "not in filter")
   end
 end
